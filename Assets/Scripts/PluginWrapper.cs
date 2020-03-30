62a007bb93b17b5cf88eeec9aadc81cdb99bff00
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2020-03-29 21:39:59 -0400

    Scan and Find Specified BLE Device

---
+++ Assets/Scripts/PluginWrapper.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class PluginWrapper : MonoBehaviour
+{
+    // For Inspector View Only
+    [Header("Hard-coded Static Variables:")]
+    [ReadOnly] public string _pluginName = "com.rutgersece.capstone2020.agoravr.blelibrary.AndroidBLE";
+    private const string pluginName = "com.rutgersece.capstone2020.agoravr.blelibrary.AndroidBLE";
+
+    [ReadOnly] [SerializeField] private string BLE_DeviceName = "BLEButton"; //TODO: Placeholder device name
+
+    private static AndroidJavaClass _pluginClass;
+    private static AndroidJavaObject _pluginInstance;
+
+    public static AndroidJavaClass PluginClass
+    {
+        get {
+            if(_pluginClass == null)
+            {
+                _pluginClass = new AndroidJavaClass(pluginName);
+            }
+            return _pluginClass;
+        }
+    }
+
+    public static AndroidJavaObject PluginInstance
+    {
+        get {
+            if(_pluginInstance == null)
+            {
+                _pluginInstance = PluginClass.CallStatic<AndroidJavaObject>("getInstance");
+            }
+            return _pluginInstance;
+        }
+    }
+
+    [Header("Textboxes:")]
+    [SerializeField] private TextMeshProUGUI BLESetupSuccess;
+    [SerializeField] private TextMeshProUGUI BLETarget;
+    [SerializeField] private TextMeshProUGUI BLEInfo;
+
+    [Header("Other Fields:")]
+    [SerializeField] private int delay = 5;
+
+    void Start()
+    {
+        checkPermissions(); // Make sure we have the correct permissions for BLE
+
+        if(Input.location.isEnabledByUser) {
+            BLEsetup();                         // Starts setup immediately if permissions are already enabled.
+        } else {
+            StartCoroutine(waitForLocation());  // Delays for 10 seconds for the user to accept location permission.
+        }
+    }
+
+    private void BLEsetup() {
+        // Get Android Activity For Context
+        AndroidJavaClass playerClass = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivityObject = playerClass.GetStatic<AndroidJavaObject> ("currentActivity");
+
+        // Setup BLE Using Plugin Calls
+        PluginInstance.Call("setContext", currentActivityObject);
+        BLESetupSuccess.text = "BLE Setup Success: " + PluginInstance.Call<bool>("BLEsetup", BLE_DeviceName).ToString();
+        BLETarget.text = "Target Device Name: " + BLE_DeviceName;
+
+        StartCoroutine(getBLEInfo());
+    }
+
+    private void checkPermissions()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation)){
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+    }
+
+    public void scanButton() {
+        PluginInstance.Call<bool>("BLEsetup", BLE_DeviceName);
+    }
+
+    IEnumerator getBLEInfo()
+    {
+        yield return new WaitForSeconds(delay);
+        BLEInfo.text = "Scanned Device Info: " + PluginInstance.Get<string>("btDeviceInfo");
+        StartCoroutine(getBLEInfo());
+    }
+
+    IEnumerator waitForLocation()
+    {
+        yield return new WaitForSeconds(10);
+        BLEsetup();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ble-unity-plugin-demo
+
+This project has a minimum API requirement of Android 7.1.1. Use any Android smartphone with the minimum requirement to run.
+
+To test, turn on the Arduino Nano 33 BLE while running @tmose1106's BLEButton script (https://github.com/Agora-VR/arduino-ble/tree/master/BLE_Button).
+
+If you have Bluetooth or Location services disabled on your phone, "BLE Setup Success" will be False. Be sure to accept the location access request when loading the app up for the first time.
+
+The plugin scans for nearby BLE devices for 10 seconds. After 10 seconds it will stop scanning (to save battery life). If the Arduino is on and running the BLE script, your device should be able to find it and output the "Scanned Device Info" data. There is a few seconds of delay to allow the plugin to find the device (doesn't actually take that long to find it, but I added the delay in case there is interference or some other factor that causes it to not find the device immediately.)
+
+If you miss the 10 second window to either accept the location permission or have the Arduino running nearby, you can press the "Scan" button at the bottom of the screen to scan for another 10 seconds.
+
+
+### Sample Logcat Output
+
+You can also monitor the Logcat output to to see the debug information.
+
+```
+D/UnityBLEPlugin: Attempting to set up Bluetooth...
+D/UnityBLEPlugin: Bluetooth set up Success!
+D/UnityBLEPlugin: Scanning for BLE Devices...
+D/UnityBLEPlugin: Callback Type: 1
+D/UnityBLEPlugin: Result: ScanResult{device=EF:4A:84:C0:81:16, scanRecord=ScanRecord [mAdvertiseFlags=6, mServiceUuids=[8bff20de-32fb-4350-bddb-afe103ef9640], mServiceSolicitationUuids=[], mManufacturerSpecificData={}, mServiceData={}, mTxPowerLevel=-2147483648, mDeviceName=BLEButton, mTransportBlocks=[]], rssi=-59, timestampNanos=151078680278137, eventType=27, primaryPhy=1, secondaryPhy=0, advertisingSid=255, txPower=127, periodicAdvertisingInterval=0}
+   . . .
+D/UnityBLEPlugin: Callback Type: 1
+D/UnityBLEPlugin: Result: ScanResult{device=EF:4A:84:C0:81:16, scanRecord=ScanRecord [mAdvertiseFlags=6, mServiceUuids=[8bff20de-32fb-4350-bddb-afe103ef9640], mServiceSolicitationUuids=[], mManufacturerSpecificData={}, mServiceData={}, mTxPowerLevel=-2147483648, mDeviceName=BLEButton, mTransportBlocks=[]], rssi=-59, timestampNanos=151088089096102, eventType=27, primaryPhy=1, secondaryPhy=0, advertisingSid=255, txPower=127, periodicAdvertisingInterval=0}
+D/UnityBLEPlugin: Stopping Scan for BLE Devices...
+```
